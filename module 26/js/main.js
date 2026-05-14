058487da8efc3aaee089a70c16ea26d96f329d83
ajanmorina57-ux <ajanmorina57@gmail.com>
2026-05-14 20:48:52 +0200

    Module 26

--- module 26/js/main.js
+++ module 26/js/main.js
@@ -22,3 +22,80 @@ var value = "Digital School";
 // After the first letter we can use digits (0 to 9), for example, value1.
 
 //  JavaScript variables are case sensitive, for example, x and X are different variables. 
+
+//var a = 50 + 50;
+
+//console.log(a);
+
+//var h = 20;
+
+//var b = 30;
+
+//var d = h + b;
+
+//console.log(d);
+
+
+//var s = 32;
+//var k = 44;
+//var h = s*k;
+//console.log(h)
+
+
+//var a = 99;
+//a++;
+//console.log(a);
+//a--;
+//console.log(a);
+
+//var x = 15;
+//x+-x;
+//console.log(x);
+
+//var x = 5;
+//var y = 5;
+
+//console.log(x == y)
+
+//console.log(x > y)
+
+//var firstName = "Digital"
+//var lastName = "School"
+
+//var fullName = firstName + "" + lastName;
+
+//console.log(fullName)
+
+
+//var school = "Digital School"
+
+//console.log('Hello from $(school)');
+
+//var age = 18;
+//console.log(age >= 18 && age <= 65);
+
+//var var1 = 5;
+//var var2 = 6;
+
+//console.log(var1 == 5  || var2 ==5);
+
+
+var var3 = 9;
+var var4 = 10;
+console.log( !(var3 == var4));
+
+var a = 10;
+var b = 5;
+
+var sum = a + b;
+var difference = a - b;
+var product = a * b;
+var division = a / b;
+
+
+console.log(`The sum of ${a} and ${b} is ${sum}`);
+console.log(`The difference of ${a} and ${b} is ${difference}`);
+console.log(`The product of ${a} and ${b} is ${product}`);
+console.log(`The division of ${a} and ${b} is ${division}`);
+
+
